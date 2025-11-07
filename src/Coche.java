3f6771e7f2edfd1e815ac876ffaa0dd269b3fa08
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2025-11-07 09:06:55 +0100

    Tercer ejercicio del bloque 2 (POO)

--- src/Coche.java
+++ src/Coche.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
+//Clase Coche con los atributos marca, modelo y anio
 public class Coche {
+    private String marca;
+    private String modelo;
+    private int anio;
+
+    //Declaramos los constructores
+    public Coche() {}
+
+    public Coche(String marca, String modelo, int anio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+
+    //Declaracion de setters y getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+    //En setAnio() se hace una restriccion para que el año no sea
+    //menor a 1886
+    public void setAnio(int anio) {
+        if(anio >= 1886) {
+            this.anio = anio;
+        }else{
+            System.out.println("El año debe ser mayor o igual a 1886");
+        }
+    }
+
+    // Metodo toString para mostrar los atributos
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "Marca='" + marca + '\'' +
+                ", Modelo='" + modelo + '\'' +
+                ", Año=" + anio +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+// Clase Main con el metodo main para ejecutar el codigo
+public class Main  {
+	public static void main(String[] args) {
+        //Declaramos dos objetos de la clase Coche, coche usando el constructor con sus atributos
+        //y coche2 con un constructor vacio
+        Coche coche = new Coche("marca1","modelo1",2000);
+        Coche coche2 = new Coche();
+        //Declaramos los atributos del objeto coche2
+        //Asegurandonos que funciona la restriccion en setAnio()
+        coche2.setMarca("marca2");
+        coche2.setModelo("modelo2");
+        coche2.setAnio(1884);
+        System.out.println(coche.toString());
+        System.out.println(coche2.toString());
+    }
 }
